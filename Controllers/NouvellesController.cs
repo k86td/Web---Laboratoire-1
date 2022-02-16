656f2c9a86eb20e64feee71046cb153e2b5a94cc
Tristan <tristanlepine14@gmail.com>
2022-02-15 20:29:02 -0500

    Ajoutez des fichiers projet.

---
+++ Controllers/NouvellesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Laboratoire_1.Models;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratoire_1.Controllers
+{
+    public class NouvellesController : Controller
+    {
+        private NouvellesDbEntities DB = new NouvellesDbEntities();
+
+        // GET: Nouvelles
+        public ActionResult Index()
+        {
+            return View(DB.Nouvelles);
+        }
+    }
+}
---
+++ Models/NouvelleView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Laboratoire_1.Models
+{
+    [MetadataType(typeof(NouvelleView))]
+    public partial class Nouvelle 
+    { }
+
+    public class NouvelleView
+    {
+        [Display(Name = "Titre de la nouvelle"), Required(ErrorMessage = "Requis")]
+        public string Titre { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date de l'ajout"), Required(ErrorMessage = "Requis")]
+        public string DateAjout { get; set; }
+
+        [Display(Name = "Texte de la nouvelle"), Required(ErrorMessage = "Requis")]
+        public string Texte { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Url vers l'image"), Required(ErrorMessage = "Requis")]
+        public string ImageUrl { get; set; }
+
+    }
+}
